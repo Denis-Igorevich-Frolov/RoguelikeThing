489b855ba79677d88326dad812e9186fd9eee86a
Denis Frolov <denisigorevichfrolov@gmail.com>
2023-05-28 17:08:55 +0400

    Реализовано перемещение по коридорам с оптимизацией их сокрытия и отображения

---
+++ Source/RoguelikeThing/RoguelikeThingGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RoguelikeThingGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ROGUELIKETHING_API ARoguelikeThingGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
