c89243be235dd29d42aa8c25bb037da5aa99b4d0
Denis Frolov <denisigorevichfrolov@gmail.com>
2023-07-12 21:28:35 +0400

    Все классы сохранения карты были убраны, создан тестовый запрос SQLite на котором и будет базироваться сохранение карты

--- Source/RoguelikeThing/RoguelikeThing.Build.cs
+++ Source/RoguelikeThing/RoguelikeThing.Build.cs
@@ -8,7 +8,7 @@ public class RoguelikeThing : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "SQLiteCore", "SQLiteSupport" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
--- Source/RoguelikeThing/Save/MapMatrix.h
+++ Source/RoguelikeThing/Save/MapMatrix.h
@@ -2,134 +2,20 @@
 
 #pragma once
 
-#include "RoguelikeThing/Map/FMapRow.h"
-#include "RoguelikeThing/Map/FMapCellStructure.h"
-#include "CoreMinimal.h"
-#include "GameFramework/SaveGame.h"
+#include "SQLiteDatabase.h"
 #include "MapMatrix.generated.h"
 
 /**
  * 
  */
 UCLASS(Blueprintable, BlueprintType)
-class ROGUELIKETHING_API UMapMatrix : public USaveGame
+class ROGUELIKETHING_API UMapMatrix : public UObject
 {
 	GENERATED_BODY()
-
-private:
-	FMapCellStructure InvalidEmptyCell;
-	TArray<FMapRow> map;
 	
 public:
-	UMapMatrix();
+	FSQLiteDatabase* db = new FSQLiteDatabase();
 
 	UFUNCTION(BlueprintCallable)
-	const FMapCellStructure& GetCell (int32 row,  int32 col);
-
-	UFUNCTION(BlueprintCallable)
-	bool SetCell (FMapCellStructure Cell,  int32 row,  int32 col);
-
-	UFUNCTION(BlueprintCallable)
-	const TArray<FMapRow>& GetMap();
-
-	UFUNCTION(BlueprintCallable)
-	void SetMap(UPARAM(ref) TArray<FMapRow>& newMap);
-
-	UFUNCTION(BlueprintCallable)
-	int32 GetRowLength();
-
-	UFUNCTION(BlueprintCallable)
-	int32 GetColLength();
-
-	///////////////////////////////////////////////////////////////////////////////////////////////
-
-	UFUNCTION(BlueprintCallable)
-	void addRow();
-
-	UFUNCTION(BlueprintCallable)
-	void addCol();
-
-	UFUNCTION(BlueprintCallable)
-	void addMultipleRows(int32 quantity);
-
-	UFUNCTION(BlueprintCallable)
-	void addMultipleCols(int32 quantity);
-
-	UFUNCTION(BlueprintCallable)
-	void enlargeRows(int32 enlargeTo);
-
-	UFUNCTION(BlueprintCallable)
-	void enlargeCols(int32 enlargeTo);
-
-	UFUNCTION(BlueprintCallable)
-	void enlargeMap(int32 enlargeRowsTo,  int32 enlargeColsTo);
-
-	///////////////////////////////////////////////////////////////////////////////////////////////
-
-	UFUNCTION(BlueprintCallable)
-	void addRowToBeginning();
-
-	UFUNCTION(BlueprintCallable)
-	void addColToBeginning();
-
-	UFUNCTION(BlueprintCallable)
-	void addMultipleRowsToBeginning(int32 quantity);
-
-	UFUNCTION(BlueprintCallable)
-	void addMultipleColsToBeginning(int32 quantity);
-
-	UFUNCTION(BlueprintCallable)
-	void enlargeRowsAtBeginning(int32 enlargeTo);
-
-	UFUNCTION(BlueprintCallable)
-	void enlargeColsAtBeginning(int32 enlargeTo);
-
-	UFUNCTION(BlueprintCallable)
-	void enlargeMapAtBeginning(int32 enlargeRowsTo,  int32 enlargeColsTo);
-
-	///////////////////////////////////////////////////////////////////////////////////////////////
-
-	UFUNCTION(BlueprintCallable)
-	void deleteLastRow();
-
-	UFUNCTION(BlueprintCallable)
-	void deleteLastCol();
-
-	UFUNCTION(BlueprintCallable)
-	void deleteMultipleLastRows(int32 quantity);
-
-	UFUNCTION(BlueprintCallable)
-	void deleteMultipleLastCols(int32 quantity);
-
-	UFUNCTION(BlueprintCallable)
-	void truncateRowsFromEnd(int32 truncateTo);
-
-	UFUNCTION(BlueprintCallable)
-	void truncateColsFromEnd(int32 truncateTo);
-
-	UFUNCTION(BlueprintCallable)
-	void truncateMapFromEnd(int32 truncateRowsTo,  int32 truncateColsTo);
-
-	///////////////////////////////////////////////////////////////////////////////////////////////
-
-	UFUNCTION(BlueprintCallable)
-	void deleteFirstRow();
-
-	UFUNCTION(BlueprintCallable)
-	void deleteFirstCol();
-
-	UFUNCTION(BlueprintCallable)
-	void deleteMultipleFirstRows(int32 quantity);
-
-	UFUNCTION(BlueprintCallable)
-	void deleteMultipleFirstCols(int32 quantity);
-
-	UFUNCTION(BlueprintCallable)
-	void truncateRowsFromBeginning(int32 truncateTo);
-
-	UFUNCTION(BlueprintCallable)
-	void truncateColsFromBeginning(int32 truncateTo);
-
-	UFUNCTION(BlueprintCallable)
-	void truncateMapFromBeginning(int32 truncateRowsTo,  int32 truncateColsTo);
+	void Test();
 };
