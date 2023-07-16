b82f89d11d5f0fd6ef9e4c153e26d473f3b3148b
Denis Frolov <denisigorevichfrolov@gmail.com>
2023-07-16 16:54:34 +0400

    Добавлен запрос на изменение данных, исправлены незакрытые транзакции

--- Source/RoguelikeThing/Map/MapMatrix.cpp
+++ Source/RoguelikeThing/Map/MapMatrix.cpp
@@ -9,16 +9,15 @@ UMapMatrix::UMapMatrix()
 
 UMapMatrix::~UMapMatrix()
 {
-    if (!mapDataBase->Close()) {
-        UE_LOG(LogTemp, Error, TEXT("Faled to close mapDataBase: %s"), *mapDataBase->GetLastError());
-    }
-    else {
+    if(LoadStatement)
+        delete LoadStatement;
+    if (mapDataBase)
         delete mapDataBase;
-    }
 }
 
 void UMapMatrix::Test()
 {
+    mapDataBase = new FSQLiteDatabase();
     mapDataBase->Open(*FilePath, ESQLiteDatabaseOpenMode::ReadWriteCreate);
 
     if (mapDataBase->IsValid()) {
@@ -29,64 +28,64 @@ void UMapMatrix::Test()
         mapDataBase->Execute(TEXT(
             "CREATE TABLE IF NOT EXISTS \"Chunk 0:0\"("
             "id INTEGER PRIMARY KEY AUTOINCREMENT,"
-            "\"Col 0\"	INTEGER,"
-            "\"Col 1\"	INTEGER,"
-            "\"Col 2\"	INTEGER,"
-            "\"Col 3\"	INTEGER,"
-            "\"Col 4\"	INTEGER,"
-            "\"Col 5\"	INTEGER,"
-            "\"Col 6\"	INTEGER,"
-            "\"Col 7\"	INTEGER,"
-            "\"Col 8\"	INTEGER,"
-            "\"Col 9\"	INTEGER,"
-            "\"Col 10\"	INTEGER,"
-            "\"Col 11\"	INTEGER,"
-            "\"Col 12\"	INTEGER,"
-            "\"Col 13\"	INTEGER,"
-            "\"Col 14\"	INTEGER,"
-            "\"Col 15\"	INTEGER,"
-            "\"Col 16\"	INTEGER,"
-            "\"Col 17\"	INTEGER,"
-            "\"Col 18\"	INTEGER,"
-            "\"Col 19\"	INTEGER,"
-            "\"Col 20\"	INTEGER,"
-            "\"Col 21\"	INTEGER,"
-            "\"Col 22\"	INTEGER,"
-            "\"Col 23\"	INTEGER,"
-            "\"Col 24\"	INTEGER,"
-            "\"Col 25\"	INTEGER,"
-            "\"Col 26\"	INTEGER,"
-            "\"Col 27\"	INTEGER,"
-            "\"Col 28\"	INTEGER,"
-            "\"Col 29\"	INTEGER,"
-            "\"Col 30\"	INTEGER,"
-            "\"Col 31\"	INTEGER,"
-            "\"Col 32\"	INTEGER,"
-            "\"Col 33\"	INTEGER,"
-            "\"Col 34\"	INTEGER,"
-            "\"Col 35\"	INTEGER,"
-            "\"Col 36\"	INTEGER,"
-            "\"Col 37\"	INTEGER,"
-            "\"Col 38\"	INTEGER,"
-            "\"Col 39\"	INTEGER,"
-            "\"Col 40\"	INTEGER,"
-            "\"Col 41\"	INTEGER,"
-            "\"Col 42\"	INTEGER,"
-            "\"Col 43\"	INTEGER,"
-            "\"Col 44\"	INTEGER,"
-            "\"Col 45\"	INTEGER,"
-            "\"Col 46\"	INTEGER,"
-            "\"Col 47\"	INTEGER,"
-            "\"Col 48\"	INTEGER,"
-            "\"Col 49\"	INTEGER,"
-            "\"Col 50\"	INTEGER"
+            "\"Col 1\"	INTEGER DEFAULT 11,"
+            "\"Col 2\"	INTEGER DEFAULT 22,"
+            "\"Col 3\"	INTEGER DEFAULT 33,"
+            "\"Col 4\"	INTEGER DEFAULT 44,"
+            "\"Col 5\"	INTEGER DEFAULT 55,"
+            "\"Col 6\"	INTEGER DEFAULT 66,"
+            "\"Col 7\"	INTEGER DEFAULT 32,"
+            "\"Col 8\"	INTEGER DEFAULT 32,"
+            "\"Col 9\"	INTEGER DEFAULT 32,"
+            "\"Col 10\"	INTEGER DEFAULT 32,"
+            "\"Col 11\"	INTEGER DEFAULT 32,"
+            "\"Col 12\"	INTEGER DEFAULT 32,"
+            "\"Col 13\"	INTEGER DEFAULT 32,"
+            "\"Col 14\"	INTEGER DEFAULT 32,"
+            "\"Col 15\"	INTEGER DEFAULT 32,"
+            "\"Col 16\"	INTEGER DEFAULT 32,"
+            "\"Col 17\"	INTEGER DEFAULT 32,"
+            "\"Col 18\"	INTEGER DEFAULT 32,"
+            "\"Col 19\"	INTEGER DEFAULT 32,"
+            "\"Col 20\"	INTEGER DEFAULT 32,"
+            "\"Col 21\"	INTEGER DEFAULT 32,"
+            "\"Col 22\"	INTEGER DEFAULT 32,"
+            "\"Col 23\"	INTEGER DEFAULT 32,"
+            "\"Col 24\"	INTEGER DEFAULT 32,"
+            "\"Col 25\"	INTEGER DEFAULT 32,"
+            "\"Col 26\"	INTEGER DEFAULT 32,"
+            "\"Col 27\"	INTEGER DEFAULT 32,"
+            "\"Col 28\"	INTEGER DEFAULT 32,"
+            "\"Col 29\"	INTEGER DEFAULT 32,"
+            "\"Col 30\"	INTEGER DEFAULT 32,"
+            "\"Col 31\"	INTEGER DEFAULT 32,"
+            "\"Col 32\"	INTEGER DEFAULT 32,"
+            "\"Col 33\"	INTEGER DEFAULT 32,"
+            "\"Col 34\"	INTEGER DEFAULT 32,"
+            "\"Col 35\"	INTEGER DEFAULT 32,"
+            "\"Col 36\"	INTEGER DEFAULT 32,"
+            "\"Col 37\"	INTEGER DEFAULT 32,"
+            "\"Col 38\"	INTEGER DEFAULT 32,"
+            "\"Col 39\"	INTEGER DEFAULT 32,"
+            "\"Col 40\"	INTEGER DEFAULT 32,"
+            "\"Col 41\"	INTEGER DEFAULT 32,"
+            "\"Col 42\"	INTEGER DEFAULT 32,"
+            "\"Col 43\"	INTEGER DEFAULT 32,"
+            "\"Col 44\"	INTEGER DEFAULT 32,"
+            "\"Col 45\"	INTEGER DEFAULT 32,"
+            "\"Col 46\"	INTEGER DEFAULT 32,"
+            "\"Col 47\"	INTEGER DEFAULT 32,"
+            "\"Col 48\"	INTEGER DEFAULT 32,"
+            "\"Col 49\"	INTEGER DEFAULT 32,"
+            "\"Col 50\"	INTEGER DEFAULT 32,"
+            "\"Col 51\"	INTEGER DEFAULT 32"
             "); "));
 
         if (!TableIsExists) {
             for (int i = 0; i < 51; i++)
                 mapDataBase->Execute(TEXT("INSERT INTO \"Chunk 0:0\" DEFAULT VALUES;"));
-            LoadStatement->Destroy();
         }
+        LoadStatement->Destroy();
 
         mapDataBase->Execute(TEXT("COMMIT;"));
     }
@@ -94,29 +93,56 @@ void UMapMatrix::Test()
         UE_LOG(LogTemp, Error, TEXT("Faled to open mapDataBase: %s"), *mapDataBase->GetLastError());
     }
 
+    if (!mapDataBase->Close()) {
+        UE_LOG(LogTemp, Error, TEXT("Faled to close mapDataBase: %s"), *mapDataBase->GetLastError());
+    }
 
-    LoadStatement->Reset();
-    mapDataBase->Execute(TEXT("BEGIN;"));
-    LoadStatement->Create(*mapDataBase, TEXT("SELECT * FROM \"Chunk 0:0\" WHERE id IS 4;"), ESQLitePreparedStatementFlags::Persistent);
-    if (mapDataBase->IsValid() && LoadStatement->IsValid()) {
-        if (LoadStatement->Execute() && LoadStatement->Step() == ESQLitePreparedStatementStepResult::Row) {
-            uint32 ii;
-            if (LoadStatement->GetColumnValueByIndex(1, ii))
-                GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, FString::Printf(TEXT("%i"), ii));
-            else
-                GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, TEXT("sASSSS"));
+    /////////////////////////
+
+    mapDataBase->Open(*FilePath, ESQLiteDatabaseOpenMode::ReadWrite);
+
+    if (mapDataBase->IsValid()) {
+        mapDataBase->Execute(TEXT(
+            "UPDATE \"Chunk 0:0\" SET \"Col 1\" = 234 WHERE id = 4;"
+            "COMMIT;"));
+
+        if (!mapDataBase->Close()) {
+            UE_LOG(LogTemp, Error, TEXT("Faled to close mapDataBase: %s"), *mapDataBase->GetLastError());
         }
-        else
-            GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, TEXT("ASSSS"));
     }
     else {
         UE_LOG(LogTemp, Error, TEXT("Faled to open mapDataBase: %s"), *mapDataBase->GetLastError());
     }
 
-    mapDataBase->Execute(TEXT("COMMIT;"));
-    LoadStatement->Destroy();
+    /////////////////////////
 
+    mapDataBase->Open(*FilePath, ESQLiteDatabaseOpenMode::ReadOnly);
 
+    if (mapDataBase->IsValid()) {
+        LoadStatement->Reset();
+        LoadStatement->Create(*mapDataBase, TEXT("SELECT * FROM \"Chunk 0:0\" WHERE id IS 4;"), ESQLitePreparedStatementFlags::Persistent);
+        if (mapDataBase->IsValid() && LoadStatement->IsValid()) {
+            if (LoadStatement->Execute() && LoadStatement->Step() == ESQLitePreparedStatementStepResult::Row) {
+                uint32 ii;
+                if (LoadStatement->GetColumnValueByIndex(1, ii))
+                    GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, FString::Printf(TEXT("%i"), ii));
+                else
+                    GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, TEXT("sASSSS"));
+            }
+            else
+                GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, TEXT("ASSSS"));
+        }
+        else {
+            UE_LOG(LogTemp, Error, TEXT("Faled to open mapDataBase: %s"), *mapDataBase->GetLastError());
+        }
+        LoadStatement->Destroy();
 
-    mapDataBase->Close();
+        if (!mapDataBase->Close()) {
+            UE_LOG(LogTemp, Error, TEXT("Faled to close mapDataBase: %s"), *mapDataBase->GetLastError());
+        }
+        mapDataBase->Execute(TEXT("COMMIT;"));
+    }
+    else {
+        UE_LOG(LogTemp, Error, TEXT("Faled to open mapDataBase: %s"), *mapDataBase->GetLastError());
+    }
 }
