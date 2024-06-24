c0b135c3192e4e9c3730e7d9626ab5cc4ae4546d
Denis Frolov <denisigorevichfrolov@gmail.com>
2024-06-25 00:54:40 +0400

    Сделана оптимизация при смещении карты влево

--- Source/RoguelikeThing/Function Libraries/TileTablesOptimizationTools.cpp
+++ Source/RoguelikeThing/Function Libraries/TileTablesOptimizationTools.cpp
@@ -46,11 +46,11 @@ void UTileTablesOptimizationTools::ChangeDisplayAreaFromShift(FVector2D TileShif
                 UE_LOG(TileTablesOptimizationTools, Warning, TEXT("old: %s, new %s"), *OldDimensions.ToString(), *CurrentDimensions.ToString());
                 for (int row = CurrentDimensions.Min.Row; row <= CurrentDimensions.Max.Row; row++) {
                     if (TilesCoordWrapper->FindWidget(row, OldDimensions.Max.Col + col + 1)) {
-                        //UE_LOG(TileTablesOptimizationTools, Warning, TEXT("fffffff r: %d, c: %d"), row, OldDimensions.Max.Col + col + 1);
+                        UE_LOG(TileTablesOptimizationTools, Warning, TEXT("fffffff r: %d, c: %d"), row, OldDimensions.Max.Col + col + 1);
                         continue;
                     }
-                    //else
-                    //    UE_LOG(TileTablesOptimizationTools, Warning, TEXT("ddddddd r: %d, c: %d"), row, OldDimensions.Max.Col + col + 1);
+                    else
+                        UE_LOG(TileTablesOptimizationTools, Warning, TEXT("ddddddd r: %d, c: %d"), row, OldDimensions.Max.Col + col + 1);
 
                     UUserWidget* Tile = TilesBuf->GetTile();
 
@@ -65,14 +65,45 @@ void UTileTablesOptimizationTools::ChangeDisplayAreaFromShift(FVector2D TileShif
 
                 for (int row = OldDimensions.Min.Row; row <= OldDimensions.Max.Row; row++) {
                     if (!TilesCoordWrapper->RemoveWidget(row, OldDimensions.Min.Col + col)) {
-                        //UE_LOG(TileTablesOptimizationTools, Warning, TEXT("fffffff r: %d, c: %d"), row, OldDimensions.Min.Col + col);
+                        UE_LOG(TileTablesOptimizationTools, Warning, TEXT("fffffff r: %d, c: %d"), row, OldDimensions.Min.Col + col);
                     }
-                    //else
-                        //UE_LOG(TileTablesOptimizationTools, Warning, TEXT("ddddddd r: %d, c: %d"), row, OldDimensions.Min.Col + col);
+                    else
+                        UE_LOG(TileTablesOptimizationTools, Warning, TEXT("ddddddd r: %d, c: %d"), row, OldDimensions.Min.Col + col);
                 }
             }
         }
 
+        if (OffsetDifference.Max.Col < 0) {
+            for (int col = 0; col > OffsetDifference.Max.Col; col--) {
+                UE_LOG(TileTablesOptimizationTools, Warning, TEXT("old: %s, new %s"), *OldDimensions.ToString(), *CurrentDimensions.ToString());
+                for (int row = CurrentDimensions.Min.Row; row <= CurrentDimensions.Max.Row; row++) {
+                    if (TilesCoordWrapper->FindWidget(row, OldDimensions.Min.Col + col - 1)) {
+                        UE_LOG(TileTablesOptimizationTools, Warning, TEXT("fffffff r: %d, c: %d"), row, OldDimensions.Min.Col + col - 1);
+                        continue;
+                    }
+                    else
+                        UE_LOG(TileTablesOptimizationTools, Warning, TEXT("ddddddd r: %d, c: %d"), row, OldDimensions.Min.Col + col - 1);
+
+                    UUserWidget* Tile = TilesBuf->GetTile();
+
+                    if (!Tile) {
+                        UE_LOG(TileTablesOptimizationTools, Warning, TEXT("AAAAAAAAAAAAA"));
+                    }
+
+                    TilesCoordWrapper->AddWidget(row, OldDimensions.Min.Col + col - 1, Tile);
+
+                    TilesGridPanel->AddChildToUniformGrid(Tile, row, OldDimensions.Min.Col + col - 1);
+                }
+
+                for (int row = OldDimensions.Min.Row; row <= OldDimensions.Max.Row; row++) {
+                    if (!TilesCoordWrapper->RemoveWidget(row, OldDimensions.Max.Col + col)) {
+                        UE_LOG(TileTablesOptimizationTools, Warning, TEXT("fffffff r: %d, c: %d"), row, OldDimensions.Max.Col + col);
+                    }
+                    else
+                        UE_LOG(TileTablesOptimizationTools, Warning, TEXT("ddddddd r: %d, c: %d"), row, OldDimensions.Max.Col + col);
+                }
+            }
+        }
     }
 
     OldDimensions = CurrentDimensions;
