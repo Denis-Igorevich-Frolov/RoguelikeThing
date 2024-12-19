c4cf3c30c58fd857cbed83de200f4edf54c47fe0
denisigorevichfrolov@gmail.com <denisigorevichfrolov@gmail.com>
2024-12-19 20:19:00 +0400

    Начата работа по созданию интерактивных объектов экспедиции

---
+++ Source/RoguelikeThing/GameObjects/ExpeditionInteractionObject.cpp
@@ -0,0 +1,27 @@
+﻿// Denis Igorevich Frolov did all this. Once there. All things reserved.
+
+
+#include "RoguelikeThing/GameObjects/ExpeditionInteractionObject.h"
+
+// Sets default values
+AExpeditionInteractionObject::AExpeditionInteractionObject()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AExpeditionInteractionObject::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AExpeditionInteractionObject::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/RoguelikeThing/GameObjects/ExpeditionInteractionObject.h
@@ -0,0 +1,26 @@
+﻿// Denis Igorevich Frolov did all this. Once there. All things reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "ExpeditionInteractionObject.generated.h"
+
+UCLASS()
+class ROGUELIKETHING_API AExpeditionInteractionObject : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AExpeditionInteractionObject();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
